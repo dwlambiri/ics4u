69fbcea84b7dc5c526b752a60c4f0098edb51b15
dwlambiri <dwlambiri@gmail.com>
2017-10-16 13:36:38 -0400

    Comments added

--- test_converter.cpp
+++ test_converter.cpp
@@ -1,34 +1,54 @@
+/*****************************************************************************
+ *	Name:   Darius W Lambiri (dwlambiri)
+ *	File:   test_converter.cpp
+ *	Course: ICS4U
+ *	Date:   Oct 16, 2017
+ *
+ *	Purpose: Reads in a numerical expression into a string
+            then it calculates and prints the result.
+ *
+ *	Usage:   The user must write a numerical expression with spaces separating
+                the numbers and operators.
+ *
+ *	Revision History:
+ *
+ *	Known Issues: If the user improperly formats their expression then the program
+                    will not be able to read it and produce the error that it
+                    cannot read the operator.
+ *
+ *****************************************************************************/
 #include <iostream>
 #include <apstring.h>
 #include <apstring.cpp>
 #include <stdlib.h>     /* atoi */
 
 int main(){
+    //Prompts user for input
     std::cout << "Enter your expression, make sure to put spaces between terms and operators" << std::endl
         << "Ex: 253 * 12" << std::endl;
     apstring superstring;
+    //Reads the user input
     getline(std::cin, superstring);
 
+    //Finds the first available space
     int spacePlace = superstring.find(' ');
     if (spacePlace <= 0){
         std::cout << "INVALID INPUT" << spacePlace << std::endl;
         return 1;
     }
-    //std::cout << "spacePlace ==" << spacePlace<< std::endl;
+    //reads the input into another string and converts it to an int
     apstring tempNumber1 = superstring.substr(0, spacePlace);
-    //std::cout << "error1 ";
     int num1 = atoi(tempNumber1.c_str());
-    //std::cout << "error2 ";
+    //finds the operator
     char operation = superstring[spacePlace + 1];
-    //std::cout << "error3 ";
+    //string used to encapsulate the second number
     apstring tempNumber2;
-    //std::cout << "error4 ";
     for (int i = spacePlace + 3; i < superstring.length() ; i++){
         tempNumber2 += superstring[i];
     }
-    //std::cout << "error5 ";
+
     int num2 = atoi(tempNumber2.c_str());
-    //std::cout << "error6 ";
+    //preforms an operation based on the user entered operator
     switch (operation){
     case '+':
         std::cout << "=" << num1 + num2 << std::endl;
